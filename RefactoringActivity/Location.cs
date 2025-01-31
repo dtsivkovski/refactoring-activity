a7e0597d6db329c296e5683db9cf21e7e982288b
uimjpmh <121411948+uimjpmh@users.noreply.github.com>
2025-01-30 19:17:30 -0800

    Finished Refactoring Location

--- RefactoringActivity/Location.cs
+++ RefactoringActivity/Location.cs
@@ -29,31 +29,51 @@ public class Location
         Location location;
         if (loc == DefaultLocations.Start)
         {
-            location = new("Start", "You are at the starting point of your adventure.");
-            location.Exits.Add("north", "Forest");
-            location.Items.Add("map");
-            location.Puzzles.Add(new Puzzle("riddle",
-                "What's tall as a house, round as a cup, and all the king's horses can't draw it up?", "well"));
+            location = GetDefaultLocationStart();
         }
         else if (loc == DefaultLocations.Forest)
         {
-            location = new("Forest", "You are in a dense, dark forest.");
-            location.Exits.Add("south", "Start");
-            location.Exits.Add("east", "Cave");
-            location.Items.Add("key");
-            location.Items.Add("potion");
+            location = GetDefaultLocationForest();
         }
         else if (loc == DefaultLocations.Cave)
         {
-            location = new("Cave", "You see a dark, ominous cave.");
-            location.Exits.Add("west", "Forest");
-            location.Items.Add("sword");
+            location = GetDefaultLocationCave();
         }
         else
         {
-            location = new("Unknown", "Unknown Location");
+            location = GetDefaultLocationUnknown();
         }
-
+        
         return location;
+    }
+    private static Location GetDefaultLocationStart()
+    {
+        Location location =new("Start", "You are at the starting point of your adventure.");
+        location.Exits.Add("north", "Forest");
+        location.Items.Add("map");
+        location.Puzzles.Add(new Puzzle("riddle",
+            "What's tall as a house, round as a cup, and all the king's horses can't draw it up?", "well"));
+        return location;
+    }
+    
+    private static Location GetDefaultLocationForest()
+    {
+        Location location = new("Forest", "You are in a dense, dark forest.");
+        location.Exits.Add("south", "Start");
+        location.Exits.Add("east", "Cave");
+        location.Items.Add("key");
+        location.Items.Add("potion");
+        return location;
+    }
+    private static Location GetDefaultLocationCave()
+    {
+        Location location = new("Cave", "You see a dark, ominous cave.");
+        location.Exits.Add("west", "Forest");
+        location.Items.Add("sword");
+        return location;
+    }
+    private static Location GetDefaultLocationUnknown()
+    {
+        return new  ("Unknown", "Unknown Location");;
     }
 }
