05819d42a73007f7b3aad1858af64645aa1d8c69
Ferdinand Paloma <Ferdinand.Paloma@gmail.com>
2025-01-30 19:31:24 -0800

    Refactored the Puzzle file

--- RefactoringActivity/Puzzle.cs
+++ RefactoringActivity/Puzzle.cs
@@ -5,14 +5,14 @@ public class Puzzle
     public string Name;
     public string Question;
     public string Answer;
-    
+        
     public Puzzle(string name, string question, string answer)
     {
-        Name = name;
-        Question = question;
-        Answer = answer;
+        setName(name);
+        this.setQuestion(question);
+        this.setAnswer(answer);
     }
-
+    
     public bool Solve()
     {
         Console.WriteLine($"Puzzle: {Question}");
@@ -20,4 +20,19 @@ public class Puzzle
         string playerAnswer = Console.ReadLine()?.ToLower();
         return playerAnswer == Answer.ToLower();
     }
+    
+    private void setAnswer(string answer)
+        {
+            Answer = answer;
+        }
+    
+        private void setQuestion(string question)
+        {
+            Question = question;
+        }
+    
+        private void setName(string name)
+        {
+            Name = name;
+        }
 }
